d26d1c128285b7ba79d176110c9a4a7936630010
Thanh2603 <86702148+Thanh2603@users.noreply.github.com>
2021-06-30 11:51:35 +0700

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _1811061832_NguyenNhatThanh_BigSchool
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Controllers/CourseController.cs
@@ -0,0 +1,45 @@
+﻿using _1811061832_NguyenNhatThanh_BigSchool.Models;
+using _1811061832_NguyenNhatThanh_BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1811061832_NguyenNhatThanh_BigSchool.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        private readonly ApplicationDbContext _dbContext;
+        public CourseController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+            {
+                var course  = new Course
+                {
+                    LecturerId = User.Identity.GetUserId(),
+                    DateTime = viewModel.GetDateTime(),
+                    CategoryId = viewModel.Category,
+                    Place = viewModel.Place
+                };
+                _dbContext.Courses.Add(course);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+        
+    }
+}
