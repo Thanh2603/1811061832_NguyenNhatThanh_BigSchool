56d62276c45a17b2ed0ed734086a50f4c2efe83c
Thanh2603 <86702148+Thanh2603@users.noreply.github.com>
2021-06-30 11:51:35 +0700

    có gì đâu tèn

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811061832_NguyenNhatThanh_BigSchool.Startup))]
+namespace _1811061832_NguyenNhatThanh_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
